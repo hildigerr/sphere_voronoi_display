6a3ffa8f40351d882ad058e09fb66afa5770c9a8
R.M. HildigerR V <moonsdad@gmail.com>
2014-05-26 11:49:03 -0700

    Label figures accuratley and supress meshes

--- sphere_voronoi_display.m
+++ sphere_voronoi_display.m
@@ -114,15 +114,35 @@ function sphere_voronoi_display ( n )
     title ( 'Figure 2: Convex Hull of Voronoi polygons -- SURFACE' );
     hold off
     
-    figure(3);
+%     figure(3);
+%     hold on
+%     trimesh(convhull(transpose(v_xyz)), transpose(v_xyz(1,:)), transpose(v_xyz(2,:)), transpose(v_xyz(3,:)));
+%     axis equal
+%     xlabel ( '-- X --' );
+%     ylabel ( '-- Y --' );
+%     zlabel ( '-- Z --' );
+%     title ( 'Figure 3: Convex Hull of Voronoi polygons -- MESH' );
+%     hold off
+    
+    figure(4);
     hold on
-    trimesh(convhull(transpose(v_xyz)), transpose(v_xyz(1,:)), transpose(v_xyz(2,:)), transpose(v_xyz(3,:)));
+    trisurf(convhull(transpose(d_xyz)), transpose(d_xyz(1,:)), transpose(d_xyz(2,:)), transpose(d_xyz(3,:)));
     axis equal
     xlabel ( '-- X --' );
     ylabel ( '-- Y --' );
     zlabel ( '-- Z --' );
-    title ( 'Figure 3: Convex Hull of Voronoi polygons -- MESH' );
+    title ( 'Figure 4: Convex Hull of MDV points -- SURFACE' );
     hold off
+    
+%     figure(5);
+%     hold on
+%     trimesh(convhull(transpose(d_xyz)), transpose(d_xyz(1,:)), transpose(d_xyz(2,:)), transpose(d_xyz(3,:)));
+%     axis equal
+%     xlabel ( '-- X --' );
+%     ylabel ( '-- Y --' );
+%     zlabel ( '-- Z --' );
+%     title ( 'Figure 5: Convex Hull of MDV points -- MESH' );
+%     hold off
     
     return
 end
